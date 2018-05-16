9881b6afe550672884a74ba383891b460884ecb0
Phil <phil@ustwo.com>
2018-05-16 21:26:14 +0100

    More icons

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,19 +16,19 @@ module.exports = {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: "Who Goes First?",
-        short_name: "whogoesfirst.app",
+        short_name: "Who Goes First?",
         start_url: "/",
         background_color: "#222",
         theme_color: "#222",
         display: "minimal-ui",
         icons: [
           {
-            src: "static/icon-128.png",
-            sizes: "128x128",
+            src: "static/icon-192.png",
+            sizes: "192x192",
           },
           {
-            src: "static/icon-256.png",
-            sizes: "256x256"
+            src: "static/icon-512.png",
+            sizes: "512x512"
           }
         ]
       },
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import Helmet from "react-helmet";
 import faviconPNG from "../../static/icon-32.png";
+import faviconPNG128 from "../../static/icon-128.png";
 import "./index.css";
 
 const Layout = ({ children, data }) => (
@@ -9,10 +10,13 @@ const Layout = ({ children, data }) => (
     <Helmet
       title={data.site.siteMetadata.title}
       meta={[
-        { name: "description", content: "Sample" },
-        { name: "keywords", content: "sample, something" }
+        {
+          name: "description",
+          content: "App to determine the first player of a board game"
+        }
       ]}
     >
+      <link rel="icon" type="image/png" sizes="128x128" href={faviconPNG128} />
       <link rel="icon" type="image/png" sizes="32x32" href={faviconPNG} />
     </Helmet>
     {children()}
