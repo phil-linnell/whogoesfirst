34acfd16de6ab2afeb38ae611eea37659d3288c7
Phil <phil@ustwo.com>
2018-05-17 12:39:34 +0100

    New Icons

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Helmet from "react-helmet";
+import faviconICO from "../../static/icon.ico";
 import faviconPNG from "../../static/icon-32.png";
 import faviconPNG128 from "../../static/icon-128.png";
 import "./index.css";
@@ -18,6 +19,7 @@ const Layout = ({ children, data }) => (
     >
       <link rel="icon" type="image/png" sizes="128x128" href={faviconPNG128} />
       <link rel="icon" type="image/png" sizes="32x32" href={faviconPNG} />
+      <link rel="shortcut icon" href={faviconICO} />
     </Helmet>
     {children()}
   </div>
