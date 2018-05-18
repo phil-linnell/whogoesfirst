383e987ca97a4efbcf77867d304494829873f4de
Phil <phil@ustwo.com>
2018-05-18 17:21:06 +0100

    Test prompt code

--- src/pages/index.js
+++ src/pages/index.js
@@ -29,18 +29,53 @@ class App extends Component {
       ready: true
     });
 
-    let deferredPrompt;
+    var deferredPrompt;
 
-    window.addEventListener('beforeinstallprompt', (e) => {
-      console.log(e);
+    window.addEventListener('beforeinstallprompt', function (e) {
       // Prevent Chrome 67 and earlier from automatically showing the prompt
       e.preventDefault();
       // Stash the event so it can be triggered later.
       deferredPrompt = e;
-      console.log("test");
+
+      showAddToHomeScreen();
+
     });
 
-    console.log(deferredPrompt);
+    function showAddToHomeScreen() {
+
+      var a2hsBtn = document.querySelector(".ad2hs-prompt");
+
+      a2hsBtn.style.display = "block";
+
+      a2hsBtn.addEventListener("click", addToHomeScreen);
+
+    }
+
+    function addToHomeScreen() {
+
+      var a2hsBtn = document.querySelector(".ad2hs-prompt");
+
+      // hide our user interface that shows our A2HS button
+      a2hsBtn.style.display = 'none';
+
+      // Show the prompt
+      deferredPrompt.prompt();
+
+      // Wait for the user to respond to the prompt
+      deferredPrompt.userChoice
+        .then(function(choiceResult){
+
+          if (choiceResult.outcome === 'accepted') {
+            console.log('User accepted the A2HS prompt');
+          } else {
+            console.log('User dismissed the A2HS prompt');
+          }
+
+          deferredPrompt = null;
+
+        });
+
+    }
   }
 
   render() {
