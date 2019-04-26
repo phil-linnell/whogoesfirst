9a8d0e07020dd4e0927b0e2a301c5f7371e8532d
Phil <phil@ustwo.com>
2019-04-26 14:27:11 +0100

    Icon and manifest stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,13 +15,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: "Start Player?",
-        short_name: "Start Player?",
+        name: "Start Player",
+        short_name: "Start Player",
         start_url: "/",
         background_color: "#222",
-        theme_color: "#222",
+        theme_color: "#111",
         display: "minimal-ui",
-        icon: "static/favicon.png"
+        icon: "static/icon-256.png"
       },
     },
     `gatsby-plugin-offline`,
